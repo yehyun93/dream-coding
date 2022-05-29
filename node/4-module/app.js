d4947ada69cc255bb445457690438eee16c12543
YeHyun <104401577+yehyun93@users.noreply.github.com>
2022-05-29 13:35:52 +0900

    폴더 변경

--- node/4-module/app.js
+++
@@ -1,4 +0,0 @@
-const counter = require('./counter.js');
-
-counter.increase();
-console.log(counter.getCount());
--- node/5-module_js/counter.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-
-export function increase() {
-  count++;
-}
-
-export function getCount() {
-  return count;
-}
