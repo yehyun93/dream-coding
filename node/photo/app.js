2ccdb79f657b7bdf0ee0d9a4c02eed7b06adc5cc
YeHyun <104401577+yehyun93@users.noreply.github.com>
2022-05-29 21:09:04 +0900

    refactoring code

--- node/photo/app.js
+++ node/photo/app.js
@@ -3,44 +3,50 @@ const path = require('path');
 
 const photo_dir_path = process.argv[2];
 
+if(!photo_dir_path) {
+  console.error('Please enter the folder name');
+  return;
+}
+
 const mkdirs = ['video', 'captured', 'duplicated'];
 
 mkdirs.forEach(dirName => {
   makeDir(dirName);
 });
 
-fs.promises.readdir(photo_dir_path, {withFileTypes: true})
-.then(data => {
-  for(let i = 0; i < data.length; i++) {
-    let fileInfo = data[i];
-    let targetDir = '';
-    let fileName = '';
-    let fileExt = checkExtension(fileInfo.name);
+fs.promises
+  .readdir(photo_dir_path, {withFileTypes: true})
+  .then(processFiles);
 
-    if(!fileInfo.isDirectory()) {
-      fileName = fileInfo.name;
+function makeDir(dirName) {
+  let mkdir_path = path.join(path.dirname(__dirname), photo_dir_path, dirName);
+
+  console.log(mkdir_path);
+
+  if(!fs.existsSync(mkdir_path)) {
+    fs.mkdir(mkdir_path, err => {console.log()});
+  }
+}
+
+function processFiles(files) {
+  console.log('process');
+  files.forEach(file => {
+    if(!file.isDirectory()){
+      let fileName = file.name;
+      let fileExt = checkExtension(file.name);
+      let targetDir = '';
 
       if(fileExt === '.png' || fileExt === '.aae') {
         targetDir = 'captured';
       } else if (fileExt === '.mov' || fileExt === '.mp4') {
         targetDir = 'video';
       }
-
       if(fileName.indexOf('E') > 0) {
         targetDir = 'duplicated';
       }
-
       movingFiles(fileName, targetDir);
     }
-  }
-});
-
-function makeDir(dirName) {
-  let mkdir_path = path.join(path.dirname(__dirname), photo_dir_path, dirName);
-
-  if(!fs.existsSync(mkdir_path)) {
-    fs.mkdir(mkdir_path, () => {});
-  }
+  });
 }
 
 function checkExtension(fileName) {
