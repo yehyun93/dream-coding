eabf65a201aef187b5282d0ebe29f97c4e609056
YeHyun <104401577+yehyun93@users.noreply.github.com>
2022-05-30 23:16:47 +0900

    test

--- node/node/1-global/app.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-console.log(global);
-
-global.hello = () => {
-  global.console.log('hello');
-}
-
-hello();
--- node/node/5-module_js/app.js
+++
@@ -1,7 +0,0 @@
-import {increase, getCount} from './counter.js';
-// 임포트 이름 지정 가능 -> counter.increase()
-// import * as counter from './counter.js';
-
-increase();
-increase();
-console.log(getCount());
